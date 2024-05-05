35775dbd651a4b58b81b11dd9f5b63b24656257c
MarineMammal <Harrisoncatonm@hotmail.com>
2024-05-05 14:30:55 +0800

    MainMechanicsWorking**
    added door and platform interaction with humming effect... barebones version, needs cleanup / visual feedback

--- Siopi/Assets/TwoDCharacterController.cs
+++ Siopi/Assets/TwoDCharacterController.cs
@@ -15,31 +15,23 @@ public class TwoDCharacterController : MonoBehaviour
     public float glideFallSpeed = 2.0f; // Speed at which the character falls while gliding
     private Rigidbody2D rb;
     private bool isGrounded = true; // Check if the character is grounded
+    public bool isOnPlatform = false; // To check if the character is on a moving platform
+    private Transform currentPlatform; // To keep track of the current platform the character is on
     private Vector2 moveInput;
     private bool isGliding = false; // Track if the character is currently gliding
 
-    // Start is called before the first frame update
     void Start()
     {
         footstepManager = GetComponentInChildren<FootstepManager>();
         rb = GetComponent<Rigidbody2D>(); // Get the Rigidbody2D component attached to the character
     }
 
-    // Update is called once per frame
     void Update()
     {
-        // Horizontal Movement
-        rb.velocity = new Vector2(moveInput.x * speed, rb.velocity.y);
-        if(rb.velocity.x == 0)
-        {
-            isMoving = false;
-        }
         RotateChildObject();
 
-        // Implement glide mechanic, only if falling (y velocity < 0)
         if (isGliding && rb.velocity.y < 0 && !isGrounded)
         {
-            // Apply a constant falling velocity to simulate gliding
             rb.velocity = new Vector2(rb.velocity.x, -glideFallSpeed);
         }
 
@@ -51,19 +43,32 @@ public class TwoDCharacterController : MonoBehaviour
         {
             footstepManager.StopFootsteps();
         }
+    }
 
+    void FixedUpdate()
+    {
+        if (isOnPlatform && currentPlatform != null)
+        {
+            // Get the current platform's velocity
+            Vector2 platformVelocity = currentPlatform.GetComponent<PlatformMove>().velocity;
+            // Apply platform velocity directly to player
+            rb.velocity = new Vector2(moveInput.x * speed + platformVelocity.x, rb.velocity.y);
+        }
+        else
+        {
+            // Regular movement without platform velocity
+            rb.velocity = new Vector2(moveInput.x * speed, rb.velocity.y);
+        }
     }
 
     void RotateChildObject()
     {
         if (rb.velocity.x > 0.1f) // Moving right
         {
-            // Set the child object rotation to face right
             ModelTransform.localEulerAngles = new Vector3(0, 0, 0);
         }
         else if (rb.velocity.x < -0.1f) // Moving left
         {
-            // Set the child object rotation to face left
             ModelTransform.localEulerAngles = new Vector3(0, 180, 0);
         }
     }
@@ -75,7 +80,6 @@ public class TwoDCharacterController : MonoBehaviour
             rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
             isGrounded = false; // Assume not grounded until collision check
         }
-        
     }
 
     public void OnRun(InputAction.CallbackContext context)
@@ -84,28 +88,39 @@ public class TwoDCharacterController : MonoBehaviour
         isMoving = moveInput.x != 0;
         isRunning = true;
     }
-   
-    // This function could be called from a separate action bound to the same jump button but with a "Hold" interaction
+
     public void OnStartGlide(InputAction.CallbackContext context)
     {
-        // Only start gliding if the player is in the air and falling
         if (context.performed && !isGrounded && rb.velocity.y < 0)
         {
             isGliding = true;
-        } else if (context.canceled)
+        }
+        else if (context.canceled)
         {
             isGliding = false;
         }
     }
 
-    // Check for collisions with the ground
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        // If the object we collided with has a tag "Ground", we are grounded
-        if (collision.gameObject.tag == "Ground")
+        if (collision.gameObject.tag == "Ground" || collision.gameObject.tag == "Platform")
         {
             isGrounded = true;
-            isGliding = false; // Stop gliding when touching the ground
+            isGliding = false;
+        }
+        if (collision.gameObject.tag == "Platform")
+        {
+            isOnPlatform = true;
+            currentPlatform = collision.transform;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Platform")
+        {
+            isOnPlatform = false;
+            currentPlatform = null;
         }
     }
 }
