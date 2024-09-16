3d4e4a409efd58fd37a8cb0b6973fcff4d3d505d
JackMA94 <127724625+JackMA94@users.noreply.github.com>
2024-09-16 17:59:03 +0800

    Updating of Branch
    Fixing of Respawn and Restart Button

---
+++ Siopi_3D/Assets/Scripts/RestartLevel.cs
@@ -0,0 +1,23 @@
+using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RestartLevel : MonoBehaviour
+{
+    public void RestartGame() 
+      {
+    		SceneManager.LoadScene(0);
+    	}
+    
+    
+
+
+ //private void Awake() 
+   // {
+     //   Time.timeScale = 1;
+     //   GameManager.health = 4;
+    //    SceneManager.LoadScene("Greybox");
+   // }
+
+}
