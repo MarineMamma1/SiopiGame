912a7095fc8c7b13f8cec4c35ab30b79407d28a1
JackMA94 <127724625+JackMA94@users.noreply.github.com>
2024-10-13 23:00:30 +0800

    Detection Range
    Did some work on detection range Script

--- Siopi_3D/Assets/Scripts/FollowScript.cs
+++ Siopi_3D/Assets/Scripts/FollowScript.cs
@@ -5,13 +5,35 @@ using UnityEngine;
 public class FollowScript : MonoBehaviour
 {
 
+    public float detectionRadius = 10.0f;
+    public float detectionAngle = 90.0f;
     public Transform targetObj;
-    // Start is called before the first frame update
-    void Start()
+
+    private void Update
     {
-        
+        //LookForPlayer();
     }
 
+    private PlayerManager LookForPlayer()
+    {
+        if (PlayerManager.Instance == null)
+        {
+            return null;
+        }
+
+        Vector3 enemyPosition = transform.position;
+        Vector3 toPlayer = PlayerManager.Instance.transform.position - enemyPosition;
+        toPlayer.y = 0;
+
+        if (toPlayer.magnitude <= detectionRadius)
+        {
+            Debug.Log("Player Has Been Detected");
+
+            return PlayerManager.Instance;
+        }
+        
+        return null;
+    }
     // Update is called once per frame
     void Update()
     {
